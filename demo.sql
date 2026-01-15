6af2b17804b3eeb24482f17d0487a06410c8bcf9
aempson-kellogg <alec.empson@kellogg.northwestern.edu>
2026-01-15 01:17:03 +0000

    Added a schema doc. Added instructions how to run

--- demo.sql
+++ demo.sql
@@ -1,4 +1,8 @@
 -- Get the first and last name of every player in the database.
 -- Note: this is a code comment in SQL (meaning it's documentation and ignored by the computer).
 
+-- To use sqlite? Type sqlite3 baseball.sqlite3 in the command terminal
+-- Type demo.sql to run
+-- CTRL + L to clear
+
 SELECT first_name, last_name FROM players;
